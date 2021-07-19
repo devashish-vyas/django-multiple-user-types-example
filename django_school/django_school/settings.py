f2fadace4da7e15747d2deb2a031c14a2e1f9d98
Devashish Vyas <devashishvyas77@gmail.com>
2021-07-19 16:41:29 +0530

    commit

--- django_school/django_school/settings.py
+++ django_school/django_school/settings.py
@@ -108,10 +108,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Custom Django auth settings
 
